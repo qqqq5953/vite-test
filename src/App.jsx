b6fe39622305222b6053385856409ef531f4d427
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-04 12:24:07 +0800

    leave only quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Quiz from './pages/Quiz'
-import Result from './pages/Result'
-import Download from './pages/Download'
+// import Result from './pages/Result'
+// import Download from './pages/Download'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />}></Route>
       <Route path="/quiz" element={<Quiz />}></Route>
-      <Route path="/result" element={<Result />}></Route>
-      <Route path="/download" element={<Download />}></Route>
+      {/* <Route path="/result" element={<Result />}></Route>
+      <Route path="/download" element={<Download />}></Route> */}
     </Routes>
   )
 }
