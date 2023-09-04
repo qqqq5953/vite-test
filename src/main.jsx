7fb82e118362d620eeb3ca01eee0c13ead50603e
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-04 14:03:32 +0800

    change to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/vite-test">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
