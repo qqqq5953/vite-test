bd91f451d5e082f641775a369cc651f9b15ab0e8
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-04 13:40:02 +0800

    delete quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Quiz from './pages/Quiz'
+// import Quiz from './pages/Quiz'
 // import Result from './pages/Result'
 // import Download from './pages/Download'
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home />}></Route>
-      <Route path="/quiz" element={<Quiz />}></Route>
+      {/* <Route path="/quiz" element={<Quiz />}></Route> */}
       {/* <Route path="/result" element={<Result />}></Route>
       <Route path="/download" element={<Download />}></Route> */}
     </Routes>
