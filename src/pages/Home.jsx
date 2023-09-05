01d5e9a36e2617ac8b454bc1a080d72a801d18c6
AndyHsieh <andy.hsieh@gaiatechs.com>
2023-09-05 10:30:47 +0800

    build test case

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,53 +4,13 @@ export default function Home() {
     function getImageUrl(name) {
         return new URL(`/src/assets/${name}.png`, import.meta.url).href
     }
-
-    function base64ToBlob(base64Data) {
-        const byteCharacters = atob(base64Data.split(",")[1]);
-        const byteNumbers = new Array(byteCharacters.length);
-        for (let i = 0; i < byteCharacters.length; i++) {
-            byteNumbers[i] = byteCharacters.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
-        return new Blob([byteArray], { type: "image/png" });
-    }
-
-    function imageUrlToBase64(imageUrl) {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.crossOrigin = "Anonymous";
-        
-            img.onload = function () {
-                const canvas = document.createElement("canvas");
-                canvas.width = img.width;
-                canvas.height = img.height;
-            
-                const ctx = canvas.getContext("2d");
-                ctx.drawImage(img, 0, 0);
-            
-                // Get the base64 data URL
-                const base64Data = canvas.toDataURL("image/png"); // You can specify the image format here
-            
-                // Resolve the Promise with the base64 data URL
-                resolve(base64Data);
-            };
-        
-            img.onerror = function () {
-                reject(new Error("Failed to load image."));
-            };
-        
-            img.src = imageUrl;
-        });
-    }
     
     const imageToShare = useRef(null)
     const output = useRef(null)
 
-    async function share(){
+    async function shareFromAPI(){
         const imageUrl = imageToShare.current.src;
         const imageBlob = await fetch(imageUrl).then((response) => response.blob());
-        // const base64Data = await imageUrlToBase64(imageUrl);
-        // const imageBlob = base64ToBlob(base64Data);
         const imageFile = new File([imageBlob], "test.png", { type: "image/png" });
 
         // Check if the browser supports the Web Share API
@@ -79,36 +39,6 @@ export default function Home() {
     const uri = isLocal ? "https://www.gaia.net/tc" : window.location.href
     const resultPageUri = encodeURI(uri)
 
-    // function fbs_click() {
-    //     const u = imageToShare.current.src;
-    //     // t=document.title;
-    //     const t = imageToShare.current.getAttribute('alt');
-    //     window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
-    //     return false;
-    // }
-
-    // function sharefbimage() {
-    //     FB.init({ appId: `your appid`, status: true, cookie: true });
-    //     FB.ui(
-    //         {
-    //             method: `share`,
-    //             name: 'Facebook Dialogs',
-    //             href: $(location).attr('href'),
-    //             link: 'https://developers.facebook.com/docs/dialogs/',
-    //             picture: 'your image url',
-    //             caption: 'Ishelf Book',
-    //             description: 'your description'
-    //         },
-    //         function (response) {
-    //             if (response && response.post_id) {
-    //                 alert('success');
-    //             } else {
-    //                 alert('error');
-    //             }
-    //         }
-    //     )
-    // };
-
     function shareit(){
         // var url = "http://www.example.com"; //Set desired URL here
         var img = imageToShare.current.src; //Set Desired Image here
@@ -118,23 +48,38 @@ export default function Home() {
     }
 
     return (
-    <>
-        ----------ig test-----------
-        <br />
-        <br />
+    <div className='mx-auto max-w-[400px] text-center px-2'>
         <img src={getImageUrl('knight_download')} alt="" width={100} ref={imageToShare} className='inline-block'/>
         <br />
         <br />
-        <button className='border border-slate-700 p-3' onClick={share} type="button">Share this image!</button>
+        <h2 className='text-xl text-blue-900 font-bold'>ig share api</h2>
+        <br />
+        <button className='border border-slate-700 p-3' onClick={shareFromAPI} type="button">IG share api</button>
+        <br />
+        <br />
+        <h2 className='text-xl text-blue-900 font-bold'>facebook share api</h2>
+        <br />
+        <button onClick={shareFromAPI} className='border border-slate-700 p-3'>FB share api</button>
         <br />
         <br />
         output: <output ref={output}></output>
         <br />
         <br />
-        ----------facebook test-----------
+        <div>------------------------------------</div>
         <br />
+        <h2 className='text-xl text-blue-900 font-bold'>facebook from sdk</h2>
         <br />
-        isLocal: {isLocal.toString()}
+        {/* facebook */}
+        <div className="fb-share-button border rounded-lg w-auto py-3" data-href={resultPageUri} data-layout="" data-size="">
+            <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${resultPageUri}`} className="fb-xfbml-parse-ignore flex items-center justify-center">
+                {/* <img src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' /> */}
+                <span className='mx-2'>FB from SDK</span>
+            </a>
+        </div>
+        <br />
+        <h2 className='text-xl text-blue-900 font-bold'>facebook from window open</h2>
+        <br />
+        <button onClick={shareit} className='border border-slate-700 p-3'>FB window open</button>
         <br />
         <br />
         window.location.href: {window.location.href}
@@ -143,20 +88,7 @@ export default function Home() {
         uri: {uri}
         <br />
         <br />
-        <button onClick={shareit}>share fb</button>
-        <br />
-        <br />
-        {/* facebook */}
-        <div className="fb-share-button border rounded-lg w-1/2 py-1.5" data-href={resultPageUri} data-layout="" data-size="">
-            <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${resultPageUri}`} className="fb-xfbml-parse-ignore flex items-center justify-center">
-                {/* <img src={getImageUrl('result-element', 'fb')} alt="facebook" className='w-8 h-8' /> */}
-                <span className='mx-2'>Facebook</span>
-            </a>
-        </div>
-        
-        <br />
-        <br />
         <div>NODE_ENV: {process.env.NODE_ENV}</div>
-    </>
+    </div>
     )
 }
